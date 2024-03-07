40b61dc319a55899a7157c459036c99829c48efd
Jason <sanchezjason@me.com>
2024-03-06 18:06:15 -0700

    swiftui/core_data/reminders- project setup

---
+++ RemindersSwiftUICoreData/RemindersSwiftUICoreData/RemindersSwiftUICoreDataApp.swift
@@ -0,0 +1,17 @@
+//
+//  RemindersSwiftUICoreDataApp.swift
+//  RemindersSwiftUICoreData
+//
+//  Created by Cause  I'm Electric on 3/6/24.
+//
+
+import SwiftUI
+
+@main
+struct RemindersSwiftUICoreDataApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
