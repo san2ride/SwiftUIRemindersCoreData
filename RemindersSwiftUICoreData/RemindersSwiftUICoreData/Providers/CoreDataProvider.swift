27baaae1eb3212ece8c41a83983100616e0bc86a
Jason <sanchezjason@me.com>
2024-03-06 18:16:25 -0700

    swiftui/core_data/reminders- CoreDataProvider

--- RemindersSwiftUICoreData/RemindersSwiftUICoreData/Providers/CoreDataProvider.swift
+++ RemindersSwiftUICoreData/RemindersSwiftUICoreData/Providers/CoreDataProvider.swift
@@ -6,3 +6,18 @@
 //
 
 import Foundation
+import CoreData
+
+class CoreDataProvider {
+    static let shared = CoreDataProvider()
+    let persistentContainer: NSPersistentContainer
+    
+    private init() {
+        persistentContainer = NSPersistentContainer(name: "RemindersModel")
+        persistentContainer.loadPersistentStores { description, error in
+            if let error {
+                fatalError("Error initializing RemindersModel\(error.localizedDescription)")
+            }
+        }
+    }
+}
