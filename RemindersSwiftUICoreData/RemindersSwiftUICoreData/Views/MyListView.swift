d243eb7dbabed22d632d22686d49e7056b430ba0
Jason <sanchezjason@me.com>
2024-03-11 13:00:27 -0600

    swiftui/core_data/reminders- navigationDestination

--- RemindersSwiftUICoreData/RemindersSwiftUICoreData/Views/MyListView.swift
+++ RemindersSwiftUICoreData/RemindersSwiftUICoreData/Views/MyListView.swift
@@ -17,19 +17,27 @@ struct MyListView: View {
                 Text("No reminders found")
             } else {
                 ForEach(myLists) { myList in
-                    VStack {
-                        MyListCellView(myList: myList)
-                            .frame(maxWidth: .infinity, alignment: .leading)
-                            .padding([.leading], 10)
-                            .font(.title3)
-                        Divider()
+                    NavigationLink(value: myList) {
+                        VStack {
+                            MyListCellView(myList: myList)
+                                .frame(maxWidth: .infinity, alignment: .leading)
+                                .padding([.leading], 10)
+                                .font(.title3)
+                            Divider()
+                        }
+                    }
+                }.scrollContentBackground(.hidden)
+                    .navigationDestination(for: MyList.self) { myList in
+                        Text(myList.name)
                     }
                 }
-            }
         }
     }
 }
 
-//#Preview {
-//    MyListView(myLists: PreviewData.myList)
-//}
+/*
+#Preview {
+    /MyListView(myLists: PreviewData.myList)
+}
+*/
+
