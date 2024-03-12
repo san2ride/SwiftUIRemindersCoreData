7d2eff682357e18df01069d434ad8c2474661896
Jason <sanchezjason@me.com>
2024-03-12 15:17:19 -0600

    swiftui/core_data/reminders- ReminderDetailView Sections

--- RemindersSwiftUICoreData/RemindersSwiftUICoreData/Views/ReminderDetailView.swift
+++ RemindersSwiftUICoreData/RemindersSwiftUICoreData/Views/ReminderDetailView.swift
@@ -9,6 +9,7 @@ import SwiftUI
 
 struct ReminderDetailView: View {
     
+    @Environment(\.dismiss) private var dismiss
     @Binding var reminder: Reminder
     @State var editConfig: ReminderEditConfig = ReminderEditConfig()
     
@@ -25,10 +26,35 @@ struct ReminderDetailView: View {
                             Image(systemName: "calendar")
                                 .foregroundColor(.red)
                         }
+                        if editConfig.hasDate {
+                            DatePicker("Select Date", selection: $editConfig.reminderDate ?? Date(), displayedComponents: .date)
+                        }
+                        Toggle(isOn: $editConfig.hasTime) {
+                            Image(systemName: "clock")
+                                .foregroundColor(.cyan)
+                        }
+                        if editConfig.hasTime {
+                            DatePicker("Select Date", selection: $editConfig.reminderTime ?? Date(), displayedComponents: .hourAndMinute)
+                        }
                     }
-                }
+                }.listStyle(.insetGrouped)
             }.onAppear {
                 editConfig = ReminderEditConfig(reminder: reminder)
+            }
+            .toolbar {
+                ToolbarItem(placement: .principal) {
+                    Text("Details")
+                        .font(.title)
+                }
+                ToolbarItem(placement: .navigationBarTrailing) {
+                    Button("Done") {
+                    }
+                }
+                ToolbarItem(placement: .navigationBarLeading) {
+                    Button("Cancel") {
+                        dismiss()
+                    }
+                }
             }
         }
     }
